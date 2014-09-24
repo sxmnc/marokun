42e956182c92b6bfffaf555fb107620916e0e8b3
kiniamaro <vdyotte@gmail.com>
2014-09-23 23:06:25 -0400

    finalize sql query

--- plugins/markovRead.js
+++ plugins/markovRead.js
@@ -17,18 +17,19 @@ module.exports = function (core) {
     }
     
     function pubListener(nick, text) {
-        words = core.util.argsToArray(text);
+        words = text.split(/[ ,]+/);
        
         if(words.length > 1){
             for (var i = 0; i < words.length -1; i++){
                 var word1 = words[i];
                 var word2 = words[i + 1];
-                
-                db.run('IF EXISTS (SELECT * WHERE pk_chain = (?,?))'
-                       +' BEGIN UPDATE chain SET n = +1 WHERE pk_chain = (?,?)'
-                       +' END ELSE BEGIN INSERT INTO chain VALUES (?,?,1)', 
-                        [word1, word2], function (err) {
-                            console.log(err);
+                console.log(word1 + " "+ word2);
+                db.run('INSERT OR REPLACE INTO chain (link1,link2,n) '
+                     + 'VALUES ($one,$two,COALESCE(((SELECT n from chain WHERE '
+                     + 'link1 = $one AND link2 = $two) + 1),1))',
+                        {$one: word1, $two: word2}, function (err) {
+                            if(err)
+                                console.log(err)
                         });        
             }    
         }
