46d35a11d689c34fe35669f4a1f859eb4a89f227
kiniamaro <vdyotte@gmail.com>
2014-09-24 02:46:28 -0400

    bot can now speak giberish

--- plugins/markovRead.js
+++ plugins/markovRead.js
@@ -20,19 +20,23 @@ module.exports = function (core) {
             console.log(err);
     }
     
+    function dbInsert(word1, word2) {
+        db.run('INSERT OR REPLACE INTO chain (link1,link2,n) '+
+               'VALUES ($one,$two,COALESCE(((SELECT n from chain WHERE '+
+               'link1 = $one AND link2 = $two) + 1),1))',
+                {$one: word1, $two: word2}, checkError);
+    }
+    
     function pubListener(nick, text) {
-        words = text.split(/[ ,]+/);
-       
-        if (words.length > 1) {
-            for (var i = 0; i < words.length -1; i++) {
+       var words = text.split(/[ ,]+/);
+       var wordsLength = words.length;
+        if (wordsLength > 1) {
+            for (var i = 0; i < wordsLength -1; i++) {
                 var word1 = words[i];
                 var word2 = words[i + 1];
-                console.log(word1 + " "+ word2);
-                db.run('INSERT OR REPLACE INTO chain (link1,link2,n) '+
-                       'VALUES ($one,$two,COALESCE(((SELECT n from chain WHERE '+
-                       'link1 = $one AND link2 = $two) + 1),1))',
-                        {$one: word1, $two: word2}, checkError(err));        
-            }    
+                dbInsert(word1, word2);       
+            }
+            dbInsert(words[wordsLength - 1], ' ');
         }
         
     }
