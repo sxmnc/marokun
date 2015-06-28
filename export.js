ccc5867d066c9c0a51fa0cb3c2c3e16c595b6e83
Alexandre Gagnon <alxgnon@gmail.com>
2015-06-27 23:32:01 -0400

    create export.js

---
+++ export.js
@@ -0,0 +1,16 @@
+var sprintf = require('util').format;
+var insert = "INSERT INTO `chain` (link1,link2,n) VALUES ('%s','%s',%s);";
+function row(a, b, c) {
+    a = a.replace(/\\/g, "\\\\").replace(/\'/g, "\\'");
+    b = b.replace(/\\/g, "\\\\").replace(/\'/g, "\\'");
+    var out = sprintf(insert, a, b, c);
+    console.log(out);
+}
+
+var sql = require('sqlite3');
+var db = new sql.Database('sqlite/MarkovDB.sqlite');
+db.all('SELECT * FROM chain', function (err, rows) {
+    rows.forEach(function (r) {
+        row(r.link1, r.link2, r.n);
+    });
+});
