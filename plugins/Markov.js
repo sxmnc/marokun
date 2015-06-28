95f473ef73b778fb2b24190911558db3f3d4fb9d
Alexandre Gagnon <alxgnon@gmail.com>
2015-06-28 00:20:12 -0400

    style nazi

--- plugins/Markov.js
+++ plugins/Markov.js
@@ -5,52 +5,52 @@ module.exports = function (core) {
     var plugin = {};
     var db = new sql.Database('MarkovDB.sqlite');
     var trigger = core.nickname;
-    
-    function getSeed(words){
+
+    function getSeed(words) {
         var seed = 0;
         var wordSeed;
-        if(words.length > 2){
-            seed = _.random(0, words.length - 2); //so we never get the last word.
+        if (words.length > 2) {
+            seed = _.random(0, words.length - 2); // so we never get the last word.
             wordSeed = [words[seed], words[seed + 1]];
         } else {
            randomPhrase();
         }
-        
-        return wordSeed;   
+
+        return wordSeed;
     }
-    
-    function randomPhrase(){
+
+    function randomPhrase() {
         db.all('SELECT * FROM chain', function (err, rows) {
             var index = _.random(0, rows.length - 1);
             var word = rows[index];
-            if(word.link2 === ' '){
+            if (word.link2 === ' ') {
                 randomPhrase();
             } else {
                 var newSeed = [word.link1, word.link2];
                 buildPhrase(newSeed, '');
-            }            
+            }
         });
     }
-    
-    function buildPhrase(seed, phrase){
+
+    function buildPhrase(seed, phrase) {
         phrase += seed[0] + " ";
-        db.all('SELECT * from chain WHERE link1 = ?',seed[1], function (err, rows) {
+        db.all('SELECT * from chain WHERE link1 = ?', seed[1], function (err, rows) {
             var index = _.random(0, rows.length - 1);
             var word = rows[index];
-            if(word.link2 === ' '){
+            if (word.link2 === ' ') {
                 phrase += seed[1] + " ";
                 core.irc.sayPub(phrase);
                 return;
             } else {
                 var newSeed = [word.link1, word.link2];
                 buildPhrase(newSeed, phrase);
-            }            
+            }
         });
     }
-    
+
     function pubListener(nick, text) {
         console.log(trigger);
-        if(core.util.containsIgnoreCase(text, trigger)) {
+        if (core.util.containsIgnoreCase(text, trigger)) {
             var words = text.split(/[ ,]+/);
             buildPhrase(getSeed(words), '');
         }
--- plugins/markovRead.js
+++ plugins/markovRead.js
@@ -3,30 +3,30 @@ var sql = require('sqlite3');
 module.exports = function (core) {
     var plugin = {};
     var db = new sql.Database('MarkovDB.sqlite');
-    
+
     function openDB() {
         db.serialize(function () {
             db.run('CREATE TABLE IF NOT EXISTS chain(link1 TEXT, link2 TEXT, n INT,'+
                    ' CONSTRAINT pk_chain PRIMARY KEY (link1,link2))');
         });
     }
-    
+
     function closeDB() {
         db.close();
     }
-    
-    function checkError(err){
+
+    function checkError(err) {
         if (err)
             console.log(err);
     }
-    
+
     function dbInsert(word1, word2) {
         db.run('INSERT OR REPLACE INTO chain (link1,link2,n) '+
                'VALUES ($one,$two,COALESCE(((SELECT n from chain WHERE '+
                'link1 = $one AND link2 = $two) + 1),1))',
                 {$one: word1, $two: word2}, checkError);
     }
-    
+
     function pubListener(nick, text) {
        var words = text.split(/[ ,]+/);
        var wordsLength = words.length;
@@ -34,11 +34,10 @@ module.exports = function (core) {
             for (var i = 0; i < wordsLength -1; i++) {
                 var word1 = words[i];
                 var word2 = words[i + 1];
-                dbInsert(word1, word2);       
+                dbInsert(word1, word2);
             }
             dbInsert(words[wordsLength - 1], ' ');
         }
-        
     }
 
     plugin.load = function () {
