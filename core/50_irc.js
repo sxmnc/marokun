ee06ed2f2a203b1b13288daab1dad1c2dbf80a31
Alexandre Gagnon <alxgnon@gmail.com>
2015-06-28 22:12:37 -0400

    restyle core/50_irc.js

--- core/50_irc.js
+++ core/50_irc.js
@@ -11,15 +11,15 @@ var irc = require('irc');
 module.exports = function (core) {
     // Initialize the irc connection.
     core.irc = new irc.Client(
-            core.server,
-            core.nickname,
-            {
-                password: core.password,
-                port: core.port,
-                realName: core.realname,
-                channels: [core.channel],
-            }
-            );
+        core.server,
+        core.nickname,
+        {
+            password: core.password,
+            port: core.port,
+            realName: core.realname,
+            channels: [core.channel],
+        }
+    );
     core.irc.setMaxListeners(100);
 
     // Add a listener that removes itself on calling `done()`.
@@ -69,18 +69,18 @@ module.exports = function (core) {
 
     // Check if a message is about a identify command suceeding.
     function identifySuccess(msg) {
-        return msg.nick &&
-            core.util.eqIgnoreCase(msg.nick, 'nickserv') &&
-            msg.args.length === 2 &&
-            _.contains(msg.args[1], 'identified');
+        return (msg.nick &&
+                core.util.eqIgnoreCase(msg.nick, 'nickserv') &&
+                msg.args.length === 2 &&
+                _.contains(msg.args[1], 'identified'));
     }
 
     // Check if a message is about a ghost command suceeding.
     function ghostingSuccess(msg) {
-        return msg.nick &&
-            core.util.eqIgnoreCase(msg.nick, 'nickserv') &&
-            msg.args.length === 2 &&
-            _.contains(msg.args[1], 'ghosted');
+        return (msg.nick &&
+                core.util.eqIgnoreCase(msg.nick, 'nickserv') &&
+                msg.args.length === 2 &&
+                _.contains(msg.args[1], 'ghosted'));
     }
 
     // Check if a message matches an error code
@@ -90,11 +90,12 @@ module.exports = function (core) {
             return false;
         }
         // Somehow, the error event isn't fired for all errors
-        var pmError = msg.commandType == 'normal' &&
-            msg.prefix.match(/\w+\.freenode\.net/) &&
-            msg.rawCommand == errorId;
-        var realError = msg.commandType == "error" &&
-            msg.rawCommand == errorId;
+        var pmError = (msg.commandType == 'normal' &&
+                       msg.prefix.match(/\w+\.freenode\.net/) &&
+                       msg.rawCommand == errorId);
+        var realError = (msg.commandType == "error" &&
+                         msg.rawCommand == errorId);
+
         return pmError || realError;
     }
 
@@ -103,18 +104,18 @@ module.exports = function (core) {
         core.irc.on('raw', function (msg) {
             if (identifySuccess(msg)) {
                 this.send('privmsg', 'nickserv',
-                        fmt('ghost %s', core.nickname));
+                          fmt('ghost %s', core.nickname));
             }
         });
 
         // Take measures if a name change returns an error.
         core.irc.on('raw', function (msg) {
-            if(verifyError(msg, core.err.nicktoofast)) {
+            if (verifyError(msg, core.err.nicktoofast)) {
                 clearTimeout(nickAbuseTimeout);
-                console.log("Nick abuse timout reset.")
-                    nickAbuseTimeout = setTimeout(function () {
-                        core.irc.setNick(msg.args[2]);
-                    }, 21000);
+                console.log("Nick abuse timout reset.");
+                nickAbuseTimeout = setTimeout(function () {
+                    core.irc.setNick(msg.args[2]);
+                }, 21000);
             } else if (verifyError(msg, core.err.nicknameinuse)) {
                 this.send('privmsg', 'nickserv', fmt('ghost %s', msg.args[1]));
             }
