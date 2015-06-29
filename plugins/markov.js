537d435c054fe721382649df6a58534339bfc1f6
Alexandre Gagnon <alxgnon@gmail.com>
2015-06-28 23:05:09 -0400

    complete refactor of plugins/markov.js

--- plugins/markov.js
+++ plugins/markov.js
@@ -3,60 +3,74 @@ var mysql = require('mysql');
 
 module.exports = function (core) {
     var plugin = {};
+
     var db = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         database: 'MarkovDB',
     });
-    var trigger = core.nickname;
-
-    function getSeed(words) {
-        var seed = 0;
-        var wordSeed;
-        if (words.length > 2) {
-            // So we never get the last word
-            seed = _.random(0, words.length - 2);
-            wordSeed = [words[seed], words[seed + 1]];
-        } else {
-            randomPhrase();
-        }
-
-        return wordSeed;
-    }
-
-    function randomPhrase() {
-        db.query('SELECT * FROM chain', function (err, rows) {
-            var index = _.random(0, rows.length - 1);
-            var word = rows[index];
-            if (word.link2 === ' ') {
-                randomPhrase();
-            } else {
-                var newSeed = [word.link1, word.link2];
-                buildPhrase(newSeed, '');
-            }
-        });
-    }
 
     function buildPhrase(seed, phrase) {
-        phrase += seed[0] + " ";
-        db.query('SELECT * from chain WHERE link1 = ?', seed[1], function (err, rows) {
-            var index = _.random(0, rows.length - 1);
-            var word = rows[index];
-            if (word.link2 === ' ') {
-                phrase += seed[1] + " ";
-                core.irc.sayPub(phrase);
-                return;
+        phrase.push(seed[0]);
+
+        var selectNext = "SELECT * FROM chain " +
+                         "WHERE link1 = ? " +
+                         "ORDER BY RAND() " +
+                         "LIMIT 1";
+        db.query(selectNext, seed[1], function (err, rows) {
+            if (rows.length === 1) {
+                var word = rows[0];
+                if (word.link2 === ' ') {
+                    phrase.push(seed[1]);
+                    core.irc.sayPub(phrase.join(" "));
+                } else {
+                    seed = [word.link1, word.link2];
+                    buildPhrase(seed, phrase);
+                }
             } else {
-                var newSeed = [word.link1, word.link2];
-                buildPhrase(newSeed, phrase);
+                console.log("buildPhrase fail");
+                console.log(phrase);
             }
         });
+    }
+
+    function randomSeedPhrase() {
+        var selectRandom = "SELECT * FROM chain " +
+                           "ORDER BY RAND() " +
+                           "LIMIT 1";
+        db.query(selectRandom, function (err, rows) {
+            if (rows.length === 1) {
+                var word = rows[0];
+                if (word.link2 === ' ') {
+                    randomSeedPhrase();
+                } else {
+                    var seed = [word.link1, word.link2];
+                    buildPhrase(seed, []);
+                }
+            } else {
+                console.log("randomSeedPhrase fail");
+            }
+        });
+    }
+
+    function getSeed(words) {
+        // So we never get the last word
+        var i = _.random(0, words.length - 2);
+        return [words[i], words[i + 1]];
+    }
+
+    function buildPhraseFor(words) {
+        if (words.length > 2) {
+            buildPhrase(getSeed(words), []);
+        } else {
+            randomSeedPhrase();
+        }
     }
 
     function pubListener(nick, text) {
-        if (core.util.containsIgnoreCase(text, trigger)) {
+        if (core.util.containsIgnoreCase(text, core.nickname)) {
             var words = text.split(/[ ,]+/);
-            buildPhrase(getSeed(words), '');
+            buildPhraseFor(words);
         }
     }
 
